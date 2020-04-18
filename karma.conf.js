1a61c47dd87ebb2e8d5579ab89c838d5c2651f3c
francknoelkouassi <francknoel.kouassi@operantic.com>
2020-04-18 11:50:35 +0200

    feat(add-file-travis): config file travis ci

--- karma.conf.js
+++ karma.conf.js
@@ -28,9 +28,13 @@ module.exports = function (config) {
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: ['HeadlessChrome'],
+		browsers: ['HeadlessChrome', 'Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
 		customLaunchers: {
 			HeadlessChrome: {
+				base: 'ChromeHeadless',
+				flags: ['--no-sandbox']
+			},
+			ChromeHeadlessNoSandbox: {
 				base: 'ChromeHeadless',
 				flags: ['--no-sandbox']
 			}
